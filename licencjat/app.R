cc52c536b52f937786e2e42efed7a52a7baebde2
agachocz <agachocz@gmail.com>
2018-05-29 17:19:35 +0200

    Cleaning code, GUI redesign.

--- licencjat/app.R
+++ licencjat/app.R
@@ -10,25 +10,14 @@ names(sym) <- c("name", "symbol")
 
 getData <- function(symbols){
   
-  #pobieranie symboli na podstawie nazw spółek
-  #stNames <- symbols <- c("CDR", "KGH", "ING", "ACP")
-  
-  #pobieranie danych
-  data <- getSymbols(paste0("WSE:",trimws(symbols[1])), verbose=TRUE, auto.assign = F, env = NULL, src='google')[,4]
-  
-  for(i in 2:length(symbols)){
-    data <- cbind(data, getSymbols(paste0("WSE:",trimws(symbols[i])), verbose=TRUE, env = NULL, auto.assign = F, src='google')[,4])
-  } 
+      file <- read.csv("not.csv", header=TRUE, sep=",", fileEncoding = "UTF-8-BOM")
+      data <- file[symbols]
   
   return(data)
   
 }
 
-
-#lambda - skłonność do ryzyka
-#n - preferowana liczba akcji
-compute <- function(lambda, n, data, r, stNames){
-  
+getReturns <- function(data){
   #liczenie stop zwrotu
   nAct <- dim(data)[2] #liczba sp??ek
   nNot <- dim(data)[1] #liczba notowa?
@@ -40,30 +29,36 @@ compute <- function(lambda, n, data, r, stNames){
     }
   }
   
+  return(ret)
+}
+
+
+#lambda - skłonność do ryzyka
+#n - preferowana liczba akcji
+compute <- function(lambda, n, data, r, stNames){
+  
+  
+  ret <- getReturns(data)
   
   #warto?ci oczekiwane
   e <- vector()
-  for(i in 1:nAct){
-    e[i] <- mean(ret[,i])
+  for(i in 1:ncol(ret)){
+    e[i] <- mean(ret[,i]) 
   }
   
   #macierz kowariancji
   cov.mat <- cov(ret)
   
   #funkcja celu
-  f <- function(w){     #w - wagi kolejnych akcji
+  f <- function(w){     #w - udziały kolejnych akcji
     
     v <- w[w >= 0]
     
     if(length(v) == 0) return(0)
     else {
-      
-      #kara za złą liczbę akcji
-      lenPun <- abs(length(v)-n)
-      
+
       e2 <- e[w >= 0]
       cov.mat2 <- cov.mat[w >= 0, w >= 0]
-      
       
       #warto?? oczekiwana portfela
       ePort <- t(e2)%*%v
@@ -73,38 +68,36 @@ compute <- function(lambda, n, data, r, stNames){
       
       #wynikowa warto?? portfela
       result <- (1-lambda)*ePort + lambda*varPort   #przemyśleć ten wzór / albo sprawdzić
+      
       #funkcja kary
-      pun <- max(0, sum(v) - 100)
+      sumPun <- abs(sum(v) - 100) #za złą sumę udziałów  
+      lenPun <- abs(length(v)-n)  #za złą liczbę akcji
       
       #ostateczny wynik
-      return(result - 3*pun - 10*lenPun)
+      return(result - 3*sumPun - 10*lenPun)
       
     }
   }
-  #n <- 3
+
+  #ograniczenia
   minV <- rep(-100, length(stNames))
   maxV <- rep(100, length(stNames))
   
   #wykorzystanie algorytmu genetycznego
   GA <- ga(type = "real-valued", 
            fitness =  f,
-           #min = c(0, 0, 0, 0, 0, 0), max = c(100, 100, 100, 100, 100, 100), - stara wersja
            min = minV,
            max = maxV, 
-           popSize = 100, maxiter = 500, run = 50)
+           popSize = 100, maxiter = 600, run = 100)
   
-  #return(GA)
-  
-  print(length(GA@solution))
-  print(length(stNames))
-  
+  #wybór tylko pierwszego rozwiązania 
+  #(jeśli jest kilka równie dobrych)
   x <- GA@solution
+  x <- x[1,]
   
   formatted <- formatResult(x, r, stNames)
   
-  #STATYSTYKI - TRZEBA UŻYWAĆ ZMODYFIKOWANYCH E I COV!!!!!!!
   return(data.frame(formatted, stats(formatted, e, cov.mat)))
-  #return(data.frame(formatted, stats(formatted)))
   
 }
 
@@ -113,13 +106,8 @@ compute <- function(lambda, n, data, r, stNames){
 #opcjonalne zaokrąglanie
 formatResult <- function(x, r, stNames){
   
-  #x <- GA@solution
-  #x <- x[x >= 0]
-  #x <- c(1, -7, 8, 4, 3, -5)
-  #stNames <- c("a", "b", "c", "d", "r", "o")
-  x <- ifelse(x < 0, 0, x)
-  #r <- TRUE
   dig <- 2
+  x <- ifelse(x < 0, 0, x)
   if(r == TRUE) {dig <- 0}
   
   maxID <- 1
@@ -129,7 +117,7 @@ formatResult <- function(x, r, stNames){
   }
   x[maxID] <- x[maxID] + 100-sum(x)
   
-  frame <- data.frame(x)
+  frame <- as.data.frame(t(x))
   names(frame) <- stNames
   
   return(frame)
@@ -155,18 +143,32 @@ stats <- function(x, e, cov.mat){
 
 
 # Interfejs użytkownika
-ui <- fluidPage(
+ui <- shinyUI(navbarPage( "Konstruowanie portfela inwestycyjnego",
   
-  # Application title
-  titlePanel("Konstruowanie portfela inwestycyjnego"),
-  
-  # Sidebar with a slider input for number of bins 
+  tabPanel("Dane",
+      sidebarLayout(
+        sidebarPanel(
+               
+               selectizeInput(
+                 "stocks", "Spółki", choices = sym[,1], multiple = TRUE
+               ),
+               radioButtons("dataType", NULL, c("Ceny akcji", "Stopy zwrotu")),
+               sliderInput("days", "Przedział czasowy",
+                           min = 1, max = 250,
+                           value = c(1,250))
+
+           ),
+        
+        mainPanel(
+          h2("Wykres stóp zwrotu"),
+          plotOutput("stocksPlot")
+        )
+        )),
+                          
+                          
+  tabPanel("Wyniki",
   sidebarLayout(
     sidebarPanel(
-      
-      selectizeInput(
-        "stocks", "Spółki", choices = sym[,1], multiple = TRUE
-      ),
       
       sliderInput("lamb",
                   "Skłonność do ryzyka: [%]",
@@ -186,7 +188,6 @@ ui <- fluidPage(
       
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       h1("Proponowany portfel inwestycyjny"),
       h3("Skłonność do ryzyka"),
@@ -200,14 +201,53 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
+))
+
 server <- function(input, output) {
   
   output$choice <- renderTable({
     data.frame(input$stocks)
-    #subset(sym, is.element(sym$name, stocks))$name
-    #subset(sym, is.element(sym$name, input$stocks))
-    #symbols <- sym %>% select(symbols) %>% filter()
+  })
+  
+  #Wykresy danych
+  output$stocksPlot <- renderPlot({
+    
+    stocks <- input$stocks
+    if(!is.null(stocks)){
+        symbols <- subset(sym, is.element(sym$name, stocks))$symbol
+        data <- getData(symbols)
+        
+        nrDays <- input$days
+        data <- data[(nrDays[1]):(nrDays[2]),]
+        data <- as.data.frame(data)
+        
+        
+        if(input$dataType == "Ceny akcji"){
+          
+                  
+          data %>% gather(key = "company", value = "price") %>%
+          cbind(day = nrDays[1]:nrDays[2]) %>%
+          ggplot(aes(x = day, y = price, color = company)) +
+          geom_line() +
+          labs(title = "Ceny akcji w badanym okresie", 
+               y = "Cena akcji", x = "Dzień", color = "Spółka")
+          
+        } else {
+          
+           ret_df <- as.data.frame(getReturns(data))
+          
+          ret_df %>% gather(key = "company", value = "returns") %>%
+            cbind(day =  nrDays[1]:(nrDays[2]-1)) %>%
+            ggplot(aes(x = day, y = returns, color = company)) +
+            geom_line() +
+            labs(title = "Stopy zwrotu w badanym okresie", 
+                 y = "Stopy zwrotu", x = "Dzień", color = "Spółka")
+        }
+
+
+      }
+      else return(NULL)
+    
   })
   
   result <- reactive({
@@ -217,8 +257,6 @@ server <- function(input, output) {
       symbols <- subset(sym, is.element(sym$name, stocks))$symbol #???
       
       data <- getData(symbols)
-      #e <- getData(stocks)
-      #e, cov.mat <- getData(stocks)
       lambda <- isolate(input$lamb/100)   #ustalona lambda
       n <- input$n           #preferowana liczba akcji
       res <- isolate(compute(lambda, n, data, input$checkbox, stocks))  
@@ -264,9 +302,6 @@ server <- function(input, output) {
   })
   
 }
-
-
-#s <- getSymbols('WSE:KGH', verbose=TRUE, src='google') 
 
 # Run the application 
 shinyApp(ui = ui, server = server)
